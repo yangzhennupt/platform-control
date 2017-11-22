4b4a6729d251ab93d77a31c5cac51d3f87434a05
杨振 <yangzhennupt@163.com>
2017-11-22 16:59:43 +0800

    改变百度地图注册方式

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App';
 import router from './router';
 import axios from 'axios';
 import ElementUI from 'element-ui';
+import BaiduMap from 'vue-baidu-map';
 //import 'element-ui/lib/theme-default/index.css';    // 默认主题
 // import '../static/css/theme-green/index.css';       // 浅绿色主题
 import 'element-ui/lib/theme-chalk/index.css'
 import "babel-polyfill";
-
+Vue.use(BaiduMap, {
+    // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
+    ak: '5SEWFRRyhHCR6tXL5LROUPWqD6B5FCGC'
+  })
 Vue.use(ElementUI);
 Vue.prototype.$axios = axios;
 new Vue({

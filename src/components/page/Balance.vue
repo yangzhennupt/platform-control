2d69f9d6b8e7ae701fb8b4376f9758c1bf5ff8e2
杨振 <yangzhennupt@163.com>
2017-11-28 20:55:23 +0800

    部分字段优化

--- src/components/page/Balance.vue
+++ src/components/page/Balance.vue
@@ -7,7 +7,7 @@
             </el-breadcrumb>
         </div>
         <div class="handle-box">
-            <el-button type="primary" icon="delete" class="handle-del mr10" @click="delAll">批量删除</el-button>
+            <el-button type="primary" icon="delete" class="handle-del mr10" @click="delAll">批量结算</el-button>
             <el-select v-model="select_cate" placeholder="筛选省份" class="handle-select mr10">
                 <el-option key="1" label="广东省" value="广东省"></el-option>
                 <el-option key="2" label="湖南省" value="湖南省"></el-option>
@@ -88,12 +88,53 @@
             },
             getData(){
                 let self = this;
-                if(process.env.NODE_ENV === 'development'){
-                    self.url = '/ms/table/list';
-                };
-                self.$axios.post(self.url, {page:self.cur_page}).then((res) => {
-                    self.tableData = res.data.list;
-                })
+                // if(process.env.NODE_ENV === 'development'){
+                //     self.url = '/ms/table/list';
+                // };
+                // self.$axios.post(self.url, {page:self.cur_page}).then((res) => {
+                //     self.tableData = res.data.list;
+                // })
+                 self.tableData =[{
+        "date": "1997-11-11",
+        "name": "林丽",
+        "address": "吉林省 辽源市 龙山区"
+    }, {
+        "date": "1987-09-24",
+        "name": "文敏",
+        "address": "江西省 萍乡市 芦溪县"
+    }, {
+        "date": "1996-08-08",
+        "name": "杨秀兰",
+        "address": "黑龙江省 黑河市 五大连池市"
+    }, {
+        "date": "1978-06-18",
+        "name": "魏强",
+        "address": "广东省 韶关市 始兴县"
+    }, {
+        "date": "1977-07-09",
+        "name": "石秀兰",
+        "address": "江苏省 宿迁市 宿豫区"
+    }, {
+        "date": "1994-09-20",
+        "name": "朱洋",
+        "address": "海外 海外 -"
+    }, {
+        "date": "1980-01-22",
+        "name": "傅敏",
+        "address": "海外 海外 -"
+    }, {
+        "date": "1985-10-10",
+        "name": "毛明",
+        "address": "内蒙古自治区 包头市 九原区"
+    }, {
+        "date": "1975-09-08",
+        "name": "何静",
+        "address": "西藏自治区 阿里地区 普兰县"
+    }, {
+        "date": "1970-06-07",
+        "name": "郭秀英",
+        "address": "四川省 巴中市 恩阳区"
+    }]
             },
             search(){
                 this.is_search = true;
--- src/components/page/BusinessAudit.vue
+++ src/components/page/BusinessAudit.vue
@@ -28,9 +28,9 @@
             <el-table-column label="操作" width="180">
                 <template slot-scope="scope">
                     <el-button size="small"
-                            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                            @click="handleEdit(scope.$index, scope.row)">通过</el-button>
                     <el-button size="small" type="danger"
-                            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                            @click="handleDelete(scope.$index, scope.row)">拒绝</el-button>
                 </template>
             </el-table-column>
         </el-table>
@@ -90,12 +90,53 @@
             },
             getData(){
                 let self = this;
-                if(process.env.NODE_ENV === 'development'){
-                    self.url = '/ms/table/list';
-                };
-                self.$axios.post(self.url, {page:self.cur_page}).then((res) => {
-                    self.tableData = res.data.list;
-                })
+                // if(process.env.NODE_ENV === 'development'){
+                //     self.url = '/ms/table/list';
+                // };
+                self.tableData =[{
+        "date": "1997-11-11",
+        "name": "林丽",
+        "address": "吉林省 辽源市 龙山区"
+    }, {
+        "date": "1987-09-24",
+        "name": "文敏",
+        "address": "江西省 萍乡市 芦溪县"
+    }, {
+        "date": "1996-08-08",
+        "name": "杨秀兰",
+        "address": "黑龙江省 黑河市 五大连池市"
+    }, {
+        "date": "1978-06-18",
+        "name": "魏强",
+        "address": "广东省 韶关市 始兴县"
+    }, {
+        "date": "1977-07-09",
+        "name": "石秀兰",
+        "address": "江苏省 宿迁市 宿豫区"
+    }, {
+        "date": "1994-09-20",
+        "name": "朱洋",
+        "address": "海外 海外 -"
+    }, {
+        "date": "1980-01-22",
+        "name": "傅敏",
+        "address": "海外 海外 -"
+    }, {
+        "date": "1985-10-10",
+        "name": "毛明",
+        "address": "内蒙古自治区 包头市 九原区"
+    }, {
+        "date": "1975-09-08",
+        "name": "何静",
+        "address": "西藏自治区 阿里地区 普兰县"
+    }, {
+        "date": "1970-06-07",
+        "name": "郭秀英",
+        "address": "四川省 巴中市 恩阳区"
+    }]
+                // self.$axios.post(self.url, {page:self.cur_page}).then((res) => {
+                //     self.tableData = res.data.list;
+                // })
             },
             search(){
                 this.is_search = true;
